5470b5eac8fcc598b8e611dc23f27e7ad0dc4c06
muhammedhamedelgaml <muhammedhamedelgaml@gmail.com>
2025-08-10 17:54:16 +0300

    Update docker.groovy

--- src/com/iti/docker.groovy
+++ src/com/iti/docker.groovy
@@ -8,10 +8,16 @@ def push(String imageName, String imageTag) {
     sh "docker push ${imageName}:${imageTag}"
 }
 
-    def login(String dockerUsername, String credentialsId = 'dockerhub-pass') {
-        steps.withCredentials([steps.string(credentialsId: credentialsId, variable: 'DOCKER_PASS')]) {
-            steps.sh """
-                echo \$DOCKER_PASS | docker login -u ${dockerUsername} --password-stdin
-            """
-        }
+    // def login(String dockerUsername, String credentialsId = 'dockerhub-pass') {
+    //     steps.withCredentials([steps.string(credentialsId: credentialsId, variable: 'DOCKER_PASS')]) {
+    //         steps.sh """
+    //             echo \$DOCKER_PASS | docker login -u ${dockerUsername} --password-stdin
+    //         """
+    //     }
+    // }
+
+def call(String dockerUsername, String docker-pass) {
+    stage("Docker login") {
+        sh "echo ${docker-pass} | docker login -u ${dockerUsername} --password-stdin"
     }
+}
