9f082325454f5381d9f02862bd6fb70ff389e048
muhammedhamedelgaml <muhammedhamedelgaml@gmail.com>
2025-08-10 15:21:54 +0300

    Update docker.groovy

--- src/com/iti/docker.groovy
+++ src/com/iti/docker.groovy
@@ -3,3 +3,15 @@ package com.iti;
 def build(IMAGE_NAME, IMAGE_TAG){
     sh "docker build -t ${IMAGE_NAME}:${IMAGE_TAG} ."
 }
+
+def push(String imageName, String imageTag) {
+    sh "docker push ${imageName}:${imageTag}"
+}
+
+def call(String dockerUsername, String passwordCredentialsId) {
+    withCredentials([string(credentialsId: passwordCredentialsId, variable: 'DOCKER_PASS')]) {
+        sh """
+            echo "\$DOCKER_PASS" | docker login -u "${dockerUsername}" --password-stdin
+        """
+    }
+}
