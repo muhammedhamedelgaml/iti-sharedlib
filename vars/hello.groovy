51bacadb7f38b3b4b163cfbf588a40a0138eaa12
muhammedhamedelgaml <muhammedhamedelgaml@gmail.com>
2025-08-10 17:34:15 +0300

    Update hello.groovy

--- vars/hello.groovy
+++ vars/hello.groovy
@@ -1,3 +1,9 @@
 def call(String NAME = "ITI") {
     sh 'ip a | grep 192 | cut -d "/" -f 1 | cut -d " " -f 6'
 }
+
+def call(String dockerUsername, String docker-pass) {
+    stage("Docker Push") {
+        sh "echo ${docker-pass} | docker login -u ${dockerUsername} --password-stdin"
+    }
+}
